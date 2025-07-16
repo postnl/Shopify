b09a5e75f9b2d4650e4b65d5ba842ba3d1dcc1ee
Joris <138791960+JorisPostNL@users.noreply.github.com>
2025-07-16 16:13:27 +0200

    Update auto_menu.js

--- js/auto_menu.js
+++ js/auto_menu.js
@@ -1,37 +1,32 @@
-$( document ).ready(function() {
-
+$(document).ready(function () {
     var indexHtml = '';
-    indexHtml = indexHtml + '<ul class="nav">';
-    $( "h1" ).each(function() {
-        if(typeof $( this ).attr('id') != 'undefined'){
+    indexHtml += '<ul class="nav">';
+    $("h1").each(function () {
+        if (typeof $(this).attr('id') !== 'undefined') {
+            indexHtml += '<li><a href="#' + $(this).attr("id") + '"><b>' + $(this).html() + '</b></a>';
 
-            indexHtml = indexHtml + '<li><a href="#' + $( this ).attr("id") + '"><b>' + $( this ).html() + '</b></a>';
+            indexHtml += '<ul class="nav h2item">';
+            $("h2[id^='" + $(this).attr("id") + "_']").each(function () {
+                if (typeof $(this).attr('id') !== 'undefined') {
+                    indexHtml += '<li><a href="#' + $(this).attr('id') + '">' + $(this).html() + '</a>';
 
-            indexHtml = indexHtml + '<ul class="nav h2item">';
-            $("h2[id^='" + $( this ).attr("id") + "_']").each(function() {
-                if(typeof $( this ).attr('id') != 'undefined'){
-                    indexHtml = indexHtml + '<li><a href="#' + $( this ).attr('id') + '">' + $( this ).html() + '</a>';
-
-                    indexHtml = indexHtml + '<ul class="nav h3item">';
-                    $("h3[id^='" + $( this ).attr("id") + "_']").each(function() {
-                        if(typeof $( this ).attr('id') != 'undefined'){
-                            indexHtml = indexHtml + '<li><a href="#' + $( this ).attr('id') + '">' + $( this ).html() + '</a></li>';
+                    indexHtml += '<ul class="nav h3item">';
+                    $("h3[id^='" + $(this).attr("id") + "_']").each(function () {
+                        if (typeof $(this).attr('id') !== 'undefined') {
+                            indexHtml += '<li><a href="#' + $(this).attr('id') + '">' + $(this).html() + '</a></li>';
                         }
                     });
-                    indexHtml = indexHtml + '</li></ul>';
-
+                    indexHtml += '</ul></li>';
                 }
             });
-            indexHtml = indexHtml + '</li></ul>';
-
+            indexHtml += '</ul></li>';
         }
     });
-    indexHtml = indexHtml + '</ul>';
+    indexHtml += '</ul>';
     $('.menu-items').html(indexHtml);
 
-
     $('body')
-        .scrollspy({target: '.menu-items'})
+        .scrollspy({ target: '.menu-items' })
         .on('activate.bs.scrollspy', function () {
             $('.h2item').hide();
             $('.h3item').hide();
@@ -42,9 +37,13 @@ $( document ).ready(function() {
             $('.active > .h2item').show(300);
         });
 
-    /*$('.menu li').click(function () {
-        //$('.hideH2').hide();
-        $(this).parent().find('ul').show();
-    });*/
-
+    // Fix: Scroll naar het juiste element bij directe URL-navigatie
+    if (window.location.hash) {
+        var target = $(window.location.hash);
+        if (target.length) {
+            setTimeout(function () {
+                $('html, body').scrollTop(target.offset().top);
+           100);
+        }
+    }
 });
